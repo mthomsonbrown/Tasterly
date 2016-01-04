f561d0125348c80d67c94c8c147f2ba26eb2f12e
ookamijin <mthomsonbrown@gmail.com>
2016-01-04 06:31:17 +0000

    Fleshed out adding friends a bit.  I need to distinguish between users that are currently friends with users that I can friend in the view, and also prevent new friendships from being made between two users that are already friends.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,7 @@
 class FriendshipsController < ApplicationController
     
     def create
+        @friendship = current_user.friendships.create! friend_id: params[:friend_id]
+        @friend = User.find(@friendship.friend_id)
     end
 end
